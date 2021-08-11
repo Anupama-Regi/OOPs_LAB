f249982b18fa29eaa5890bf148c6afe07be67904
Anupama-Regi <74719646+Anupama-Regi@users.noreply.github.com>
2021-08-11 21:23:46 +0530

    Update Square_area.java

--- graphics_package/Square_area.java
+++ graphics_package/Square_area.java
@@ -1,9 +1,5 @@
 package graphics_package;
-public class Square_area
+public interface Square_area
 {
-	public float square (float a)
-	{
-		System.out.println("\n\nArea of Square : ");
-		return (a*a);
-	}
+	public void square(float a);
 }
