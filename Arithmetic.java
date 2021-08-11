c3d22080dbbf529339c5a13f07326167b42282a7
Anupama-Regi <74719646+Anupama-Regi@users.noreply.github.com>
2021-08-11 21:19:57 +0530

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,7 +1,17 @@
 import arithmetic_package.*;
 import java.util.*;
-public class Arithmetic
+public class Arithmetic implements arithmetic_package.Sum,arithmetic_package.Multiplication
 {
+	public void arith_sum_operator(int a,int b)
+	{
+		System.out.println("\n\nSum of 2 number : ");
+		System.out.println(a+b);
+	}
+	public void arith_multi_operator(int a,int b)
+	{
+		System.out.println("\n\nMultiplication of 2 number : ");
+		System.out.println(a*b);
+	}
 	public static void main(String args[])
 	{
 
@@ -12,8 +22,8 @@ public class Arithmetic
 		int a=sc.nextInt();
 		System.out.println("Enter the second number for addition : ");
 		int b=sc.nextInt();
-		Sum su=new Sum();
-		System.out.println(su.arithoperator(a,b));
+		Arithmetic su=new Arithmetic();
+		su.arith_sum_operator(a,b);
 
 		System.out.println("\n\t\t-------------------------------------");
 		System.out.println("\n\nEnter first number for Subtraction : ");
@@ -21,7 +31,7 @@ public class Arithmetic
 		System.out.println("Enter the second number for Subtraction : ");
 		int b1=sc.nextInt();
 		Subtract sub=new Subtract();
-		System.out.println(sub.arithoperator(a1,b1));
+		System.out.println(sub.arith_sub_operator(a1,b1));
 
 		System.out.println("\n\t\t-------------------------------------");
 		System.out.println("\n\nEnter first number for division : ");
@@ -29,14 +39,14 @@ public class Arithmetic
 		System.out.println("Enter the second number for division : ");
 		int b2=sc.nextInt();
 		Division div=new Division();
-		System.out.println(div.arithoperator(a2,b2));
+		System.out.println(div.arith_div_operator(a2,b2));
 
 		System.out.println("\n\t\t-------------------------------------");
 		System.out.println("\n\nEnter first number for multiplication : ");
 		int a3=sc.nextInt();
 		System.out.println("Enter the second number for multiplication : ");
 		int b3=sc.nextInt();
-		Multiplication mul=new Multiplication();
-		System.out.println(mul.arithoperator(a3,b3));
+		Arithmetic mul=new Arithmetic();
+		mul.arith_multi_operator(a3,b3);
 	}
 }
